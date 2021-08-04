389b2133561f3204d80be57d4c3dbb9e31084154
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-05 08:17:03 +0900

    gitignore 추가 + 서버 index 기본 세팅

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 
 2021-08-05 server 작업 폴더 생성, package.json 스크립트 설정, client 생성(client readme 별도 작성)
 - nodemon을 사용하는 스크립트에서 오류가 발생하는 분은 nodemon을 글로벌로 설치하시기바랍니다.
+
+## 백엔드 개발 스크립트
+    npm run backend
+    
+그냥 npm run start를 할 경우 nodemon을 사용하지 않습니다.
+
+## 프론트 개발 스크립트 
+    npm run frontend
+
+## 종합 환경 개발 스크립트
+    npm run dev
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-console.log('hello server index');
+import express from "express";
+// 추가해야 할 모듈 및 미들웨어 : path, cors, cookie-parser, config, mongoose, Routers, routes
+
+const app = express();
+
+
+// 여기서 미들웨어 세팅
+// app.use(cors()); // CORS 도입 후 주석 해제
+
+// 여기서 라우팅 설정(dev)
+app.get('/', (req,res) => {
+    res.send('First Routing');
+});
+
+if(process.env.NODE_ENV === "production") {
+    // 빌드 배포 환경에서의 라우팅 설정
+
+    app.use(express.static("clicent/build"));
+
+    app.get("*", (req, res) => { 
+        // res.sendFile()
+     })
+}
+
+// 서버 리스닝
+
+const port = process.env.PORT || 5000
+
+function HandleListening() {
+    console.log(`✅ http://localhost:${port} 에서 서버 리스닝에 성공했습니다.`)
+}
+
+app.listen(port, HandleListening);
