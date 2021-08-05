8be614b11ec2fe352b3aebf35b2a0d5448a103dd
msiqoc <edison98@naver.com>
2021-08-05 16:48:03 +0900

    qna

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 // schema
 var answerSchema = mongoose.Schema({
-  title:{type:String, required:true},
-  body:{type:String, required:true},
-  author: String, //익명여부 구현해야함.
-  createdAt:{type:Date, default:Date.now},
-  updatedAt:{type:Date},
+    title:{type:String, required:true},
+    body:{type:String, required:true},
+    author: String, //익명여부 구현해야함.
+    createdAt:{type:Date, default:Date.now},
+    updatedAt:{type:Date},
 });
 
 // model & export
--- server/models/question.js
+++ server/models/question.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 // schema
 var questionSchema = mongoose.Schema({
-  title:{type:String, required:true},
-  body:{type:String, required:true},
-  author: String, //익명여부 구현해야함.
-  createdAt:{type:Date, default:Date.now},
-  updatedAt:{type:Date},
+    title:{type:String, required:true},
+    body:{type:String, required:true},
+    author: String, //익명여부 구현해야함.
+    createdAt:{type:Date, default:Date.now},
+    updatedAt:{type:Date},
 });
 
 // model & export
