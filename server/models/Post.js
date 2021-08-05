29f76c1545c508c305adc95db2758ac262441215
msiqoc <edison98@naver.com>
2021-08-05 16:20:58 +0900

    일단스키마

---
+++ server/models/Post.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+// schema
+var postSchema = mongoose.Schema({
+  title:{type:String, required:true},
+  body:{type:String, required:true},
+  //nickname:{type:String, required:true},
+  createdAt:{type:Date, default:Date.now},
+  updatedAt:{type:Date},
+});
+
+// model & export
+var Post = mongoose.model('post', postSchema);
+module.exports = Post;
