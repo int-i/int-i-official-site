5456779d496c727042dfdf0fbea9e85a83d7c0db
msiqoc <edison98@naver.com>
2021-08-05 16:45:33 +0900

    qna

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,14 +1,13 @@
 var mongoose = require('mongoose');
-
 // schema
-var postSchema = mongoose.Schema({
+var answerSchema = mongoose.Schema({
   title:{type:String, required:true},
   body:{type:String, required:true},
-  //nickname:{type:String, required:true},
+  author: String, //익명여부 구현해야함.
   createdAt:{type:Date, default:Date.now},
   updatedAt:{type:Date},
 });
 
 // model & export
-var Post = mongoose.model('post', postSchema);
+var Post = mongoose.model('answer', answerSchema);
 module.exports = Post;
