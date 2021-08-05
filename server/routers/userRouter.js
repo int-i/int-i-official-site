5f69146aea1083bbd92e7305e1f29cc9c0783648
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-05 22:00:33 +0900

    라우터에 컨트롤러 import

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "./routes";
-import {} from "../controllers/userController";
+import { Users } from "../controllers/userController";
 
 const userRouter = express.Router();
 
