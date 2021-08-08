b96a9b7f05b012377a7faea9587691d2a784ee23
msiqoc <edison98@naver.com>
2021-08-08 14:37:06 +0900

    질문게시판의 질문스키마와 답변스키마 구현

--- server/models/Answer.js
+++ server/models/Answer.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 //질문 게시판에서 답변의 스키마
 const answerSchema = new mongoose.Schema({
 
-    //작성자 익명여부와 유저 아이디 가져오는거 구현해야함.
+    
     //답변작성자, 답변 제목, 답변 내용, 답변 작성날짜
     author: String, 
 
