7691825a1b1858e4760834bae6f9a63121589e6e
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-10 02:13:25 +0900

    temp

--- client/src/components/view/MainPage/MainPage.js
+++ client/src/components/view/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 //import { useState } from "react";
 import style from "./MainPage.module.scss";
+import "./arrow.scss";
 import "antd/dist/antd.css";
 import { Carousel } from "antd";
 import banner1 from "../../../assets/images/banner/banner1.png";
@@ -8,20 +9,29 @@ const MainPage = () => {
 	return (
 		<div className={style.banner}>
 			<Carousel
-			autoplay={true}
-			autoplaySpeed={10000} // ms 단위
+				arrows={true}
+				autoplay={true}
+				autoplaySpeed={10000} // ms 단위
 			>
 				<div>
 					<a href="/" /*target="_blank"*/>
-						<img src={banner1} alt={"인트아이 배너"} style={{ backgroundColor: "black"}} />
+						<img
+							src={banner1}
+							alt={"인트아이 배너"}
+							style={{ backgroundColor: "black" }}
+						/>
 					</a>
 				</div>
 				<div>
-					<img src={banner1} alt={"인트아이 배너"} style={{ backgroundColor: "black" }} />
+					<img
+						src={banner1}
+						alt={"인트아이 배너"}
+						style={{ backgroundColor: "black" }}
+					/>
 				</div>
 			</Carousel>
 		</div>
 	);
-}
+};
 
 export default MainPage;
