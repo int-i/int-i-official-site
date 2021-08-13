8882bde3363bea365fd9aca9d0a65f6aaa195abe
msiqoc <edison98@naver.com>
2021-08-13 21:41:42 +0900

    read방식 수정, 질문 하나 삭제되면 답변 다 삭제되도록 구현, 좋아요 구현

--- backend/controllers/codeAController.js
+++ backend/controllers/codeAController.js
@@ -1,29 +1,33 @@
-import Answer from "../models/Answer";
+import CodeRepositoryA from "../models/CodeRepositoryA";
+import Like from "../models/Like";
 
 /*
  * create : 답변 작성이 끝나고 클라이언트가 등록 버튼을 눌렀을 때 데이터 전달
- * 지금은 하나의 답변에 연관된 question의 id를 req.body로 직접 받아오지만
+ * 지금은 하나의 답변에 연관된 코드저장소 문제의 id(codeq)를 req.body로 직접 받아오지만
  * 나중에 프론트랑 연결이 되면 이 부분은 살짝 수정될 수도 있음.
  */
 export const PostAnswer = async (req, res) => {
 	const user = req.user;
-	const { question, contents, anonymous, createdAt } = req.body;
+	const { codeq, contents, recommend, createdAt } = req.body;
 
     if (!contents) {
         return res.status(400).json({ addAnswer: false, reason: "contents is required" });
     }
+	else if (user.role !== 1) {
+		return res.status(400).json({ addAnswer: false, reason: "uploader must be the member of IntI" })
+	}
 
 	// 등록이 잘 됐을 때 성공 메세지 보내고 안되면 에러 메세지 보내기.
 	try {
 		
-		await Answer.create({
+		await CodeRepositoryA.create({
             author: user.nickname,
+			codeq,
 			contents,
-			anonymous,
-			question,
+			recommend,
 			createdAt
         });
-
+		//console.log(codeq);
 		res.status(200).json({ addAnswer: true });
 	} catch (error) {
 		res.status(400).send({ error: error.message })
@@ -34,7 +38,7 @@ export const PostAnswer = async (req, res) => {
 export const GetAllAnswers = async (req, res) => {
 
 	try{
-		const answers = await Answer.find({});
+		const answers = await CodeRepositoryA.find({});
 		res.status(200).json({ answers: answers });
 	} catch (error) {
 		res.status(400).send({ error: error.message });
@@ -48,23 +52,55 @@ export const GetAllAnswers = async (req, res) => {
  */
 export const PostEditAnswer = async (req, res) => {
 	const user = req.user;
-	const { _id, contents, anonymous, createdAt } = req.body;
+	const { _id, contents, createdAt } = req.body;
 
 	if (!contents) {
-        return res.status(400).json({ updateAnswer: false, reason: "content is required" });
+        return res.status(400).json({ updateAnswer: false, reason: "contents is required" });
     }
 
 	// 수정이 잘 됐을 때 성공 메세지 보내고 안되면 에러 메세지 보내기.
 	try {
 
 		// 자동으로 생성된 아이디로 게시글을 찾고 업데이트 시켜준다.
-        await Answer.findByIdAndUpdate(_id, { $set: { author: user.nickname, contents: contents, anonymous: anonymous, createdAt: createdAt }});
+        await CodeRepositoryA.findByIdAndUpdate(_id, { $set: { author: user.nickname, contents: contents, createdAt: createdAt }});
         res.status(200).json({ updateAnswer: true });
 	} catch (error) {
 		res.status(400).send({ error: error.message })
 	}
 };
 
+/*
+ * update : 추천수 올리기
+ * delete와 똑같이 추천 버튼 클릭을 하면 _id값을 전달해서 추천수를 올리는게 가능하도록
+ * post 방식으로 구현
+ */
+export const PostRecommend = async (req, res, next) => {
+	const user = req.user;
+	const { _id } = req.body;
+
+	try {
+		const isLiked = await Like.findOne({ $and: [{ nickname: user.nickname }, { qoraId: _id }] });
+		const answer = await CodeRepositoryA.findOne({ _id });
+
+		if (isLiked) {
+			await CodeRepositoryA.findByIdAndUpdate( _id, { $set: { recommend: answer.recommend - 1 } });
+			await Like.deleteOne({ $and: [{ nickname: user.nickname }, { codeqId: _id }] });
+		}
+		else {
+			await CodeRepositoryA.findByIdAndUpdate( _id, { $set: { recommend: answer.recommend + 1 } });
+		
+			await Like.create({
+				nickname: user.nickname,
+				codeqId: _id
+			});
+
+		}
+		
+        return res.status(200).json({ updateRecommendSuccess: true });
+    } catch (err) {
+        next(err);
+    }
+};
 
 // delete: 하나의 답변을 삭제하는 것.
 export const PostDeleteAnswer = async (req, res, next) => {
--- backend/controllers/codeQController.js
+++ backend/controllers/codeQController.js
@@ -1,4 +1,5 @@
 import CodeRepositoryQ from "../models/CodeRepositoryQ";
+import CodeRepositoryA from "../models/CodeRepositoryA";
 import Like from "../models/Like";
 
 // create : 문제 작성이 끝나고 클라이언트가 등록 버튼을 눌렀을 때 데이터 전달
@@ -30,7 +31,6 @@ export const PostQuestion = async (req, res) => {
 	}
 }
 
-
 // read: 모든 질문들 다 보여주기
 export const GetAllQuestions = async (req, res) => {
 	try {
@@ -43,11 +43,14 @@ export const GetAllQuestions = async (req, res) => {
 
 /*
  * read : 게시판에서 특정 게시글을 눌렀을 때 해당 게시글 정보 보여주기.
- * 아직 어떤 데이터를 받아서 보여줘야할지 안정했기 때문에 나중에 다시 구현.
+ * 변경사항 : post방식으로 사용자가 게시글을 클릭했을 때 해당 게시글의 _id를 찾아서 그 게시글의 정보를 다 보여주는 식으로 구현.
  */
  export const GetOneQuestion = async (req, res) => {
+	const { _id } = req.body;
+
 	try {
-        return res.status(200).json({ getQuestion: true });
+        const question = await CodeRepositoryQ.findById({ _id });
+        return res.status(200).json({ question: question });
     } catch (err) {
         next(err);
     }
@@ -59,7 +62,7 @@ export const GetAllQuestions = async (req, res) => {
  */
 export const PostEditQuestion = async (req, res) => {
 	const user = req.user;
-	const { _id, title, contents, createdAt } = req.body;
+	const { _id, title, contents, anonymous, createdAt } = req.body;
 
 	if (!title || !contents) {
 		console.log("400: title and contents both are required in code repository question");
@@ -67,7 +70,7 @@ export const PostEditQuestion = async (req, res) => {
     }
 
 	try {
-        await CodeRepositoryQ.findByIdAndUpdate( _id, { $set: { title: title, contents: contents, createdAt: createdAt }});
+        await CodeRepositoryQ.findByIdAndUpdate( _id, { $set: { author: user.nickname, anonymous: anonymous, title: title, contents: contents, createdAt: createdAt }});
         res.status(200).json({ updateQuestion: true });
 	} catch (error) {
 		//console.log("error occured while updating a question of code repository (update): "+error);
@@ -85,18 +88,19 @@ export const PostRecommend = async (req, res, next) => {
 	const { _id } = req.body;
 
 	try {
-		const isLiked = await Like.findOne({ $and: [{ nickname: user.nickname }, { codeqId: _id }] });
+		const isLiked = await Like.findOne({ $and: [{ nickname: user.nickname }, { qoraId: _id }] });
+		const question = await CodeRepositoryQ.findOne({ _id });
 
 		if (isLiked) {
 			await CodeRepositoryQ.findByIdAndUpdate( _id, { $set: { recommend: question.recommend - 1 } });
-			await Like.deleteOne({ $and: [{ nickname: user.nickname }, { codeqId: _id }] });
+			await Like.deleteOne({ $and: [{ nickname: user.nickname }, { qoraId: _id }] });
 		}
 		else {
 			await CodeRepositoryQ.findByIdAndUpdate( _id, { $set: { recommend: question.recommend + 1 } });
 		
 			await Like.create({
 				nickname: user.nickname,
-				codeqId: _id
+				qoraId: _id
 			});
 
 		}
@@ -108,11 +112,12 @@ export const PostRecommend = async (req, res, next) => {
 };
 
 
-// delete: 하나의 포스트를 삭제하는 것.
+// delete: 하나의 코드저장소 문제를 삭제하는 것.
 export const PostDeleteQuestion = async (req, res, next) => {
 	const { _id } = req.body;
 
 	try {
+		await CodeRepositoryA.deleteMany({ codeq: _id });
         await CodeRepositoryQ.deleteOne({ _id });
         return res.status(200).json({ delQuestionSuccess: true });
     } catch (err) {
