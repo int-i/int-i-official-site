03344c484cfd08e3793f7bc0f3542f92b9dde5a5
Seungeunlee <dmsdl0630@naver.com>
2021-08-14 17:59:00 +0900

    프로필 수정 페이지 디자인

--- frontend/src/components/view/ProfileEditPage/ProfileEditPage.js
+++ frontend/src/components/view/ProfileEditPage/ProfileEditPage.js
@@ -4,13 +4,25 @@ import React, { useState, useRef } from "react";
 import styles from "./ProfileEditPage.module.scss";
 import FormBtn from "../FormBtn/FormBtn";
 
+function Tags({ tags }) {
+	return (
+		<span>
+			<span className={styles.tagstyle}>{tags.tag}</span>
+			&nbsp;&nbsp;
+		</span>
+	);
+}
+
 const ProfileEditPage = (props) => {
-	const [nickname, SetNickname] = useState("");
-	const [email, SetEmail] = useState("");
-	const [password, SetPassword] = useState("");
-	const [name, SetName] = useState("");
-	const [studentId, SetStudentId] = useState("");
-	const [interest, SetInterest] = useState("");
+	const [nickname, SetNickname] = useState("닉네임");
+	const [email, SetEmail] = useState("12345@12345");
+	const [password, SetPassword] = useState("비밀번호");
+	const [name, SetName] = useState("이름");
+	const [studentId, SetStudentId] = useState("12345678");
+	const [interest, SetInterest] = useState(""); // 관심 분야
+
+	const [tags, SetTags] = useState([{ tag: "JavaScript" }]); // 관심 태그
+
 	const [about, SetAbout] = useState(""); // 자기소개
 	const [github, SetGithub] = useState("");
 	const [blog, SetBlog] = useState("");
@@ -77,6 +89,10 @@ const ProfileEditPage = (props) => {
 		SetInterest(event.currentTarget.value);
 	};
 
+	const OnTagsHandler = (event) => {
+		SetTags(event.currentTarget.value);
+	};
+
 	const OnAboutHandler = (event) => {
 		SetAbout(event.currentTarget.value);
 	};
@@ -111,10 +127,11 @@ const ProfileEditPage = (props) => {
 	return (
 		<center className="profileEditCenter">
 			<div className={[styles.profileEditPage, "NanumSquare"].join(" ")}>
-				<form id="register" onSubmit={OnSubmitHandler}>
+				<div className={styles.title}>프로필 정보 수정</div>
+				<form id="profileEdit" onSubmit={OnSubmitHandler}>
 					<table className={styles.tablestyle}>
 						<tbody>
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>
 									<span style={{ color: "red" }}>*</span>
 									&nbsp;닉네임
@@ -137,7 +154,7 @@ const ProfileEditPage = (props) => {
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>
 									<span style={{ color: "red" }}>*</span>
 									&nbsp;이메일
@@ -153,7 +170,7 @@ const ProfileEditPage = (props) => {
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>
 									<span style={{ color: "red" }}>*</span>
 									&nbsp;비밀번호
@@ -177,7 +194,7 @@ const ProfileEditPage = (props) => {
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>
 									<span style={{ color: "red" }}>*</span>
 									&nbsp;이름
@@ -200,57 +217,103 @@ const ProfileEditPage = (props) => {
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>학번</td>
 								<td className={styles.td}>
 									<input
 										className={styles.inputstyle}
 										value={studentId}
 										onChange={OnStudentIdHandler}
+										placeholder={"학번을 등록해보세요"}
 									/>
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>관심분야</td>
 								<td className={styles.td}>
 									<input
 										className={styles.inputstyle}
 										value={interest}
 										onChange={OnInterestHandler}
+										placeholder={"관심 분야를 등록해보세요"}
 									/>
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
-								<td className={styles.td}>자기소개</td>
+							<tr>
+								<td className={styles.td}>관심태그</td>
 								<td className={styles.td}>
+									<span
+										style={{ margin: "25px 0px 20px 0px" }}
+									>
+										{tags.map((tag, index) => (
+											<Tags tags={tag} key={index} />
+										))}
+									</span>
 									<input
-										className={styles.inputstyle}
+										className={styles.tagstyle}
+										//value={tags}
+										onChange={OnTagsHandler}
+										placeholder={"관심 태그를 등록해보세요"}
+									/>
+									&nbsp;&nbsp;
+									<button
+										type="button"
+										className={styles.tagbutton}
+										//onClick={onCreateTag}
+									>
+										추가
+									</button>
+									&nbsp;
+									<button
+										type="button"
+										className={styles.tagbutton}
+										//onClick={onDeleteTag}
+									>
+										삭제
+									</button>
+								</td>
+							</tr>
+
+							<tr>
+								<td className={styles.abouttd}>자기소개</td>
+								<td className={styles.abouttd}>
+									<textarea
+										className={styles.aboutinputstyle}
 										value={about}
 										onChange={OnAboutHandler}
+										placeholder={
+											"자신에 대한 간단한 소개를 등록해보세요"
+										}
 									/>
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>깃허브</td>
 								<td className={styles.td}>
 									<input
 										className={styles.inputstyle}
 										value={github}
 										onChange={OnGithubHandler}
+										placeholder={
+											"깃허브 주소를 등록해보세요"
+										}
 									/>
 								</td>
 							</tr>
 
-							<tr className={styles.tr}>
+							<tr>
 								<td className={styles.td}>블로그</td>
 								<td className={styles.td}>
 									<input
 										className={styles.inputstyle}
 										value={blog}
 										onChange={OnBlogHandler}
+										placeholder={
+											"블로그 주소를 등록해보세요"
+										}
 									/>
 								</td>
 							</tr>
@@ -271,14 +334,14 @@ const ProfileEditPage = (props) => {
 				/>
 				<FormBtn
 					type="submit"
-					form="register"
+					form="profileEdit"
 					width="120px"
 					height="45px"
 					borderRadius="10px"
 					fontSize="18px"
 					margin="5px"
 					kind="컬러"
-					text="수정완료"
+					text="수정 완료"
 				/>
 			</div>
 		</center>
