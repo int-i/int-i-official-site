b2f872815878289a3a50f64c4b70ac525641f429
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-15 19:16:59 +0900

    일단 댓글까지 구현 완료 답변 구현해야함

--- frontend/src/components/view/PostViewPage/PostViewPage.js
+++ frontend/src/components/view/PostViewPage/PostViewPage.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PostViewPage.module.scss";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Viewer } from "@toast-ui/react-editor";
+import DefaultProfile from "../../../assets/images/icon/하얀 유저.png";
 import test from "../../temp.json";
+import Comment from "../Comment/Comment";
 function PostViewPage() {
 	return (
 		<div className={[styles.PostViewContainer, "Spoqa"].join(" ")}>
 			<h1>{test.title}</h1>
+
 			<div className={styles.postInfo}>
 				<div className={styles.infoLeft}>
 					<div>{test.author}</div>
@@ -18,7 +21,31 @@ function PostViewPage() {
 					<button>삭제</button>
 				</div>
 			</div>
+
 			<Viewer initialValue={test.content} />
+
+			{/* 좋아요를 누르기 전과 누른 상태에서 컬러가 달라야함 */}
+			<div className={styles.likeContaienr}>
+				<button className={styles.likeButton}>❤</button>
+				<div className={styles.likeNumber}> 5 </div>
+			</div>
+
+			{/* 유저 프로필이 없으면 기본 프로필, 있으면 프로필 사진을 */}
+			{test.profile !== "" ? (
+				<div className={styles.profileContainer}>
+					<img className={styles.profileImage} src={test.profile} alt="profile" />
+					<div className={styles.profileName}>{test.author}</div>
+				</div>
+			) : (
+				<div className={styles.defaultProfileContainer}>
+					<div className={styles.imageBackground}>
+						<img className={styles.defaultImage} src={DefaultProfile} alt="profile" />
+					</div>
+					<div className={styles.profileName}>{test.author}</div>
+				</div>
+			)}
+
+			<Comment />
 		</div>
 	);
 }
